4d630bf413926ac107600affe36d5c68eb724e59
Rolf Krahl <rolf@rotkraut.de>
2015-11-22 22:50:06 +0100

    Add some widgets to control ConstraintBacktrack.

--- ct-rt.py
+++ ct-rt.py
@@ -13,10 +13,12 @@ from racetrack.linalg import *
 from racetrack.track import Track
 from racetrack.tk import TrackView
 from racetrack.car import Car
-from racetrack.backtrack import SlowMotionBacktrack
+from racetrack.backtrack import SlowMotionBacktrack, ConstraintBacktrack
+from racetrack.exception import NoSolutionError
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
+
 
 class Application(Frame):
 
@@ -29,8 +31,12 @@ class Application(Frame):
         log.info('Search a simple path using SlowMotionBacktrack ...')
         backtrack = SlowMotionBacktrack(self.car)
         backtrack.search()
-        log.info('Found a path with %d steps.' % len(self.car.path))
-        self.trackview.drawPath(self.car.path, tags=['car'])
+        self.solution = list(self.car.path)
+        log.info('Found a solution with %d steps.' % (len(self.solution) - 1))
+        self.redrawTrack()
+        maxsteos = len(self.solution) - 1
+        self.prefixlen.set(str(maxsteos))
+        self.backtrack = ConstraintBacktrack(self.car, maxsteps=maxsteos)
 
     def createTrack(self):
         log.info('Create track ...')
@@ -94,9 +100,55 @@ class Application(Frame):
                                   command=self.trackview.zoomOut, 
                                   accelerator="Ctrl--")
 
+        buttonframe = Frame(self)
+        buttonframe.grid(row=2, column=0, columnspan=2)
+        self.prefixlen = StringVar()
+        isIntCommand = self.register(self.isInt)
+        self.prefixentry = Entry(buttonframe, textvariable=self.prefixlen,
+                                 width=6, validate='all',
+                                 validatecommand=(isIntCommand, '%P'))
+        self.prefixentry.grid(row=0, column=0, padx=10, pady=2)
+        self.nextbutton = Button(buttonframe, text='Next', width=9,
+                                 command=self.searchNext)
+        self.nextbutton.grid(row=0, column=1, padx=10, pady=2)
+
         self.bind_all("<Control-q>", lambda e:self.quit())
         self.bind_all("<Control-plus>", self.trackview.zoomIn)
         self.bind_all("<Control-minus>", self.trackview.zoomOut)
+        self.prefixentry.bind('<Return>', self.setPrefix)
+
+    def isInt(self, value):
+        if value == "":
+            return True
+        try:
+            int(value)
+            return True
+        except ValueError:
+            return False
+
+    def redrawTrack(self):
+        self.trackview.delete('car')
+        self.trackview.drawPath(self.car.path, tags=['car'])
+
+    def setPrefix(self, event):
+        prefix = int(self.prefixlen.get())
+        log.info('set prescribed initial path len = %d.' % prefix)
+        self.car.reset(prefix)
+        self.redrawTrack()
+        maxsteps = len(self.solution) - 1
+        self.backtrack = ConstraintBacktrack(self.car, maxsteps=maxsteps)
+
+    def searchNext(self):
+        log.info('Search a path using ConstraintBacktrack ...')
+        log.info('max step = %d.' % self.backtrack.maxsteps)
+        try:
+            self.backtrack.searchNextSolution()
+            self.solution = self.car.path[:]
+            log.info('Found a solution with %d steps.' % (len(self.solution)-1))
+            self.redrawTrack()
+        except NoSolutionError:
+            log.info('No solution found.')
+            self.car.path = self.solution[:]
 
 app = Application()
 app.mainloop()
